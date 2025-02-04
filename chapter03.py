22566e97b62021ca846e2c6b2adc2a739e821260
SivertLog <SivertLog@gmail.com>
2025-02-04 10:48:03 -0800

    chapter03 rev

--- chapter03.py
+++ chapter03.py
@@ -5,20 +5,22 @@ letter = "L"
 number = 5
 triangle(letter, number)
 
-def print_right(word):
-    a=40-len(word)
+def print_right(word, num):
+    a=num-len(word)
     for i in range(a):
         print(" ", end="")
     print(word)
 
-def print_border():
-    for i in range(40):
-        print("*", end="")
+def print_border(char, num):
+    for i in range(num):
+        print(char, end="")
     print()
 
+ch="*"
+le=40
 right_word=input("enter a word: ")
-print_border()
-print_right(right_word)
+print_border(ch, le)
+print_right(right_word, le)
 
 print()
 
@@ -44,3 +46,48 @@ def coolrectangle(l, la, h, w):
                 print(la, end="")
             print(l)
 coolrectangle("=", "H", 4, 8)
+
+print()
+
+#############
+##beer song##
+#############
+def plural_check(num): #corrects bottle vs bottles
+    if num == 1:
+        p=""
+    else:
+        p="s"
+    return p
+
+def bottle_verse(count): #main verse
+    s=plural_check(count)
+    print(f"{count} bottle{s} of beer on the wall")
+    print(f"{count} bottle{s} of beer")
+    if count-1 == 0: #checks for end of song
+        print("Take it down, pass it around")
+        print("No more bottles of beer on the wall")
+    else: #business as usual
+        print("Take one down, pass it around")
+        s=plural_check(count-1)
+        print(f"{count-1} bottle{s} of beer on the wall")
+
+bottles=99
+bottle_verse(bottles)
+
+print()
+print_border(ch, le)
+
+answer=input("Do you want to hear the whole song? (y/n): ").lower()
+ch="#"
+le=30
+if answer == "y":
+    print_border(ch, le)
+    print_right("The Beer Song", le)
+    print_border(ch, le)
+    for i in range(99, 0, -1):
+        bottle_verse(i)
+        print()
+else:
+    print_border(ch, le)
+    print_right("Lol me neither!", le)
+    print_border(ch, le)
